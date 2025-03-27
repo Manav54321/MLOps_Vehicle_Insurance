cecb0e3218daaa2a198d0d04670aecb372dc524f
manav <manavdesai170303@gmail.com>
2025-03-27 11:12:46 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Manav Desai",
+    author_email="manavdesai170303@gmail.com",
+    packages=find_packages()
+)
